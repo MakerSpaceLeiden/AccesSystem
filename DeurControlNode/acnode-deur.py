8ce748e0a72ca42f0723e550a4bb8f116b682e4b
Dirk-Willem van Gulik <dirkx@webweaving.org>
2016-03-24 07:42:43 +0100

    Shorten window for replay attack, add debug logging

--- DeurControlNode/acnode-deur.py
+++ DeurControlNode/acnode-deur.py
@@ -75,60 +75,60 @@ stepper=0
 steps=350
 
 # In BCM mode
-pin_dir=17	
+pin_dir=17     
 pin_step =27
 pin_enable=22
 Sleep=0.0001
 
 def init_gpio():
-	if args.offline:
-	   logging.info("TEST: init_gpio()")
-	   return
-	GPIO.setwarnings(False)
-	GPIO.setmode(GPIO.BCM)
-	if ( stepper == 1 ):
-		logging.debug("setup stepper")
-		GPIO.setup(pin_dir, GPIO.OUT)
-		GPIO.setup(pin_step, GPIO.OUT)
-		GPIO.setup(pin_enable, GPIO.OUT)
-		GPIO.output(pin_enable, True)
-		GPIO.output(pin_dir, False)
-		GPIO.output(pin_step, False)
-	if ( mosfet == 1 ):
-		GPIO.setup(18, GPIO.OUT)
-		GPIO.output(18, False)
+     if args.offline:
+        logging.info("TEST: init_gpio()")
+        return
+     GPIO.setwarnings(False)
+     GPIO.setmode(GPIO.BCM)
+     if ( stepper == 1 ):
+          logging.debug("setup stepper")
+          GPIO.setup(pin_dir, GPIO.OUT)
+          GPIO.setup(pin_step, GPIO.OUT)
+          GPIO.setup(pin_enable, GPIO.OUT)
+          GPIO.output(pin_enable, True)
+          GPIO.output(pin_dir, False)
+          GPIO.output(pin_step, False)
+     if ( mosfet == 1 ):
+          GPIO.setup(18, GPIO.OUT)
+          GPIO.output(18, False)
 
 def open_door():
-	if args.offline:
-	   logging.debug("TEST: open_door()")
-	   return
-	if ( mosfet == 1 ): 
-		logging.debug("open via mosfet")
-    		GPIO.output(18, True)	
-    		time.sleep(5)
-    		GPIO.output(18, False)
-	if (stepper == 1 ):
-		logging.debug("open via stepper")
-		GPIO.output(pin_enable,False)
-#	        step_stepper()
-		for step in xrange(steps):
-			GPIO.output(pin_dir,False)	
-			GPIO.output(pin_step,True)
-			GPIO.output(pin_step,False)
-			time.sleep(0.0009)
-				
-   		time.sleep(0.01)
-		for step in xrange(steps):
-			GPIO.output(pin_dir,True)	
-			GPIO.output(pin_step,True)
-			time.sleep(Sleep)
-			GPIO.output(pin_step,False)
-			time.sleep(0.0009)
-				
-		GPIO.output(pin_dir,False)	
-		time.sleep(0.1)
-#	        step_stepper()d
-		GPIO.output(pin_enable,True)
+     if args.offline:
+        logging.debug("TEST: open_door()")
+        return
+     if ( mosfet == 1 ): 
+          logging.debug("open via mosfet")
+          GPIO.output(18, True)     
+          time.sleep(5)
+          GPIO.output(18, False)
+     if (stepper == 1 ):
+          logging.debug("open via stepper")
+          GPIO.output(pin_enable,False)
+#             step_stepper()
+          for step in xrange(steps):
+               GPIO.output(pin_dir,False)     
+               GPIO.output(pin_step,True)
+               GPIO.output(pin_step,False)
+               time.sleep(0.0009)
+                    
+          time.sleep(0.01)
+          for step in xrange(steps):
+               GPIO.output(pin_dir,True)     
+               GPIO.output(pin_step,True)
+               time.sleep(Sleep)
+               GPIO.output(pin_step,False)
+               time.sleep(0.0009)
+                    
+          GPIO.output(pin_dir,False)     
+          time.sleep(0.1)
+#             step_stepper()d
+          GPIO.output(pin_enable,True)
 
 init_gpio()
 forever= True
@@ -141,6 +141,24 @@ def end_read(signal,frame):
 
 signal.signal(signal.SIGINT, end_read)
 signal.signal(signal.SIGQUIT, end_read)
+
+def roll_nonce():
+   nonce = hashlib.sha256(os.urandom(1024)).hexdigest()
+
+   secret = cnf['secret']
+
+   data = "roll"
+
+   HMAC = hmac.new(secret.encode('ASCII'),nonce.encode('ASCII'),hashlib.sha256)
+   HMAC.update(data.encode('ASCII'))
+   hexdigest = HMAC.hexdigest()
+
+   data = "SIG/1.00 " + hexdigest + " " + nonce + " " + cnf['node'] + " " + data
+   topic = cnf['mqtt']['sub']+"/" + master + "/" + cnf['node']
+
+   logging.debug("Sending @"+topic+": "+data)
+   publish.single(topic, data, hostname=cnf['mqtt']['host'], protocol="publish.MQTTv311")
+
 
 def on_connect(client, userdata, flags, rc):
     logging.info(("(re)Connected with result code "+str(rc)))
@@ -215,6 +233,7 @@ def on_message(client, userdata, message):
       return
 
     open_door()
+    roll_nonce()
 
 client = mqtt.Client()
 
@@ -228,27 +247,12 @@ client.on_connect = on_connect
 client.on_subscribe= on_subscribe
 
 while forever:
-        if time.time() - nonce_time > renew:
-            nonce = hashlib.sha256(os.urandom(1024)).hexdigest()
-            nonce_time = time.time()
+  client.loop()
+  if time.time() - nonce_time > renew:
+     roll_nonce()
+     logging.info("Rolling secret")
+     nonce_time = time.time()
 
-            secret = cnf['secret']
-
-            data = "roll"
-
-            HMAC = hmac.new(secret.encode('ASCII'),nonce.encode('ASCII'),hashlib.sha256)
-            HMAC.update(data.encode('ASCII'))
-            hexdigest = HMAC.hexdigest()
-
-            data = "SIG/1.00 " + hexdigest + " " + nonce + " " + cnf['node'] + " " + data
-
-	    topic = cnf['mqtt']['sub']+"/" + master + "/" + cnf['node']
-
-            publish.single(topic, data, hostname=cnf['mqtt']['host'], protocol="publish.MQTTv311")
-
-            logging.info("Rolling secret")
-
-	client.loop()
 client.disconnect()
 
 if not args.offline:
