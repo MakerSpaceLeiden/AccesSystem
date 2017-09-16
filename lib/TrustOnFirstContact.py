29770cc5fa5e47e0293cb5a2cde1021f0a508005
Dirk-Willem van Gulik <dirkx@webweaving.org>
2017-09-16 16:07:13 +0200

    Enough rudimentary functionality for trust on first contact/use to check interop with Arduino/ESP12F.

--- lib/TrustOnFirstContact.py
+++ lib/TrustOnFirstContact.py
@@ -4,6 +4,8 @@
 import time
 import sys
 import os
+import base64 
+import socket
 
 import axolotl_curve25519 as curve
 
@@ -32,7 +34,7 @@ class TrustOnFirstContact(ACNode):
 
        randm32 = Random.new().read(32)
        self.cnf.privatekey = curve.generatePrivateKey(randm32)
-       self.cnf.publickey == curve.generatePublicKey(private_key_master)
+       self.cnf.publickey = curve.generatePublicKey(self.cnf.privatekey)
 
        if self.cnf.privatekeyfile:
            if 32 != open(self.cnf.privatekeyfile,"wb").write(self.cnf.privatekey):
@@ -41,7 +43,7 @@ class TrustOnFirstContact(ACNode):
 
     if self.cnf.privatekey:
           # base64 decode it - and check it is 32 bytes.
-         self.cnf.privatekey = base64.b64decode(self.cnf.privatekey)
+         # self.cnf.privatekey = base64.b64decode(self.cnf.privatekey)
 
          if len(self.cnf.privatekey) != 32:
              self.logger.critical("Command line private key not exactly 32 bytes. aborting.");
@@ -59,46 +61,48 @@ class TrustOnFirstContact(ACNode):
          sys.exit(1);
  
   def parseArguments(self):
-    self.parser.add('--privatekey',
+    self.parser.add('--privatekey','-K',
          help='Private Curve25519 key (Default: unset, auto generated')
-    self.parser.add('--privatekeyfile',
+    self.parser.add('--privatekeyfile','-f',
          help='Private Curve25519 key file (Default: unset, auto generated')
-    self.parser.add('--newprivatekey',
+    self.parser.add('--newprivatekey','-N',action='count',
          help='Generate a new private key, safe to privatekeyfile if defined')
-    self.parser.add('--tofconly','-F',
+    self.parser.add('--tofconly','-F',action='count',
          help='Force trust-on-first-use mode (default: off)')
 
     super().parseArguments()
 
   def announce(self,dstnode):
     if self.cnf.privatekey:
-    	return self.send(dstnode, "announce " + socket.gethostbyname(socket.gethostname()) + " " + base64.b64encode(self.cnf.privatekey)
+        bp = base64.b64encode(self.cnf.privatekey).decode('ASCII')
+        return self.send(dstnode, "announce " + socket.gethostbyname(socket.gethostname()) + " " + bp)
 
-    return super.announce(dstnode):
+    return super().announce(dstnode)
 
   def cmd_announce(self,path,node,theirbeat,payload):
     try:
        cmd, ip, pubkey = payload.split(" ")
        pubkey = base64.b64decode(pubkey)
        if len(pubkey) != 32:
-             self.logger.error("Ignoring malformed private key of node {}".format(node));
+             self.logger.error("Ignoring malformed private key of node {}".format(node))
              return None
 
        if self.pubkeys[ node ]:
-             if self.pubkey[node] == pubkey:
-                  self.logger.debug("Ignoring (unchanged) private key of node {}".format(node));
+          if self.pubkey[node] == pubkey:
+                  self.logger.debug("Ignoring (unchanged) private key of node {}".format(node))
+                  return super().cmd_announce()
                   return None
 
-             self.logger.info ("Ignoring (changed) private key of node {}".format(node));
-             return None
-
-          self.logger.info ("Learned a private key of node {} on first contact.".format(node));
-	  self.pubkeys[ node ] = pubkey
-     except e:
-          self.logger.error("Error parsing announce. Ignored.");
+          self.logger.info ("Ignoring (changed) private key of node {}".format(node))
           return None
-    
-    return super.cmd_announce()
+
+       self.logger.info("Learned a private key of node {} on first contact.".format(node))
+       self.pubkeys[ node ] = pubkey
+    except:
+       self.logger.error("Error parsing announce. Ignored.");
+       return None
+
+    return super().cmd_announce()
 
 # Allow this class to auto instanciate if
 # we run it on its own.
