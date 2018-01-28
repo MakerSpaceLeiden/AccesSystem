ad6deebbbdbdd67cba05a18fabb61b0bf267a2de
Aart Schipper <aart@Aarts-iMac.fritz.box>
2018-01-28 15:38:19 +0100

    POESP V1.0 first commit

---
+++ POESP_board/ETH_LAN8720/ETH_LAN8720.ino
@@ -0,0 +1,99 @@
+/*
+    This sketch shows the Ethernet event usage
+    First light Aart 10-12-2017
+*/
+
+#define ETH_PHY_ADDR      1
+#define ETH_PHY_MDC       23
+#define ETH_PHY_MDIO      18
+#define ETH_PHY_POWER     17
+#define ETH_PHY_TYPE      ETH_PHY_LAN8720
+
+#include <ETH.h> 
+
+static bool eth_connected = false;
+
+void WiFiEvent(WiFiEvent_t event)
+{
+  switch (event) {
+    case SYSTEM_EVENT_ETH_START:
+      Serial.println("ETH Started");
+      //set eth hostname here
+      ETH.setHostname("esp32-ethernet");
+      break;
+    case SYSTEM_EVENT_ETH_CONNECTED:
+      Serial.println("ETH Connected");
+      break;
+    case SYSTEM_EVENT_ETH_GOT_IP:
+      Serial.print("ETH MAC: ");
+      Serial.print(ETH.macAddress());
+      Serial.print(", IPv4: ");
+      Serial.print(ETH.localIP());
+      if (ETH.fullDuplex()) {
+        Serial.print(", FULL_DUPLEX");
+      }
+      Serial.print(", ");
+      Serial.print(ETH.linkSpeed());
+      Serial.println("Mbps");
+      eth_connected = true;
+      break;
+    case SYSTEM_EVENT_ETH_DISCONNECTED:
+      Serial.println("ETH Disconnected");
+      eth_connected = false;
+      break;
+    case SYSTEM_EVENT_ETH_STOP:
+      Serial.println("ETH Stopped");
+      eth_connected = false;
+      break;
+    default:
+      Serial.println("Unknown event");
+      break;
+  }
+}
+
+void testClient(const char * host, uint16_t port)
+{
+  Serial.print("\nconnecting to ");
+  Serial.println(host);
+
+  WiFiClient client;
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    return;
+  }
+  client.printf("GET / HTTP/1.1\r\nHost: %s\r\n\r\n", host);
+  while (client.connected() && !client.available());
+  while (client.available()) {
+    Serial.write(client.read());
+  }
+
+  Serial.println("closing connection\n");
+  client.stop();
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Start");
+ 
+  delay(5000);
+  
+  Serial.println("Init");
+  WiFi.onEvent(WiFiEvent);
+  
+  Serial.println("begin");
+  ETH.begin();
+  
+  Serial.println("done");
+}
+
+
+void loop()
+{
+  Serial.println("Tock");
+
+  if (eth_connected) {
+    testClient("google.com", 80);
+  }
+  delay(1000);
+}
