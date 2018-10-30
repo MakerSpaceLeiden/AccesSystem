22996e0db9a279939ebdcaeabcdb2bcb1495c551
Dirk-Willem van Gulik <dirkx@webweaving.org>
2018-10-30 20:56:58 +0100

    Various test addded

--- test/PoE-test/PowerBoard-Basisboard-v1.1/Basic-Tests/Basic-Tests.ino
+++ test/PoE-test/PowerBoard-Basisboard-v1.1/Basic-Tests/Basic-Tests.ino
@@ -1,19 +1,31 @@
 // https://wiki.makerspaceleiden.nl/mediawiki/index.php/Powernode_1.1
+//
 #define AART_LED        (GPIO_NUM_16)
 #define RELAY           (GPIO_NUM_5)
 #define CURRENT_COIL    (GPIO_NUM_15)
-#define SW2             (GPIO_NUM_2)
+#define SW1             (GPIO_NUM_2)
+#define SW2             (GPIO_NUM_39)
+#define OPTO1           (GPIO_NUM_34)
+#define OPTO12          (GPIO_NUM_35)
 
 void setup() {
-  Serial.begin(115200);
-  Serial.print("Booting ");
+
+  Serial.begin(9600);
+  delay(250);
+  Serial.print("\n\n\n\nBooting ");
   Serial.println(__FILE__);
   Serial.println(__DATE__ " " __TIME__);
 
-  // put your setup code here, to run once:
   pinMode(AART_LED, OUTPUT);
   pinMode(RELAY, OUTPUT);
-  pinMode(CURRENT_COIL, INPUT); // analog
+  pinMode(CURRENT_COIL, ANALOG); // analog
+
+#ifdef SW1
+  pinMode(SW1, INPUT); 
+#endif
+#ifdef SW2
+  pinMode(SW2, INPUT); 
+#endif
 }
 
 void loop() {
@@ -51,11 +63,39 @@ void loop() {
 
 #ifdef SW2
   {
-    static unsigned long lastStateSW2 = 0;
-    if (digitalRead(SW2) != lastStateSW2) {
+    static unsigned long last = 0;
+    if (digitalRead(SW2) != last) {
       Serial.printf("Current state SW2: %d\n", digitalRead(SW2));
-      lastStateSW2 = digitalRead(SW2);
+      last = digitalRead(SW2);
     };
+  }
+#endif
+#ifdef SW1
+  {
+    static unsigned long last = 0;
+    if (digitalRead(SW1) != last) {
+      Serial.printf("Current state SW1: %d\n", digitalRead(SW1));
+      last = digitalRead(SW1);
+    };
+  }
+#endif
+
+#ifdef OPTO1
+  {
+     static unsigned last = 0; 
+     if (millis() - last > 1000) {
+      last = millis();
+      Serial.printf("OPTO 1: %d\n", analogRead(OPTO1));
+     }     
+  }
+#endif
+#ifdef OPTO2
+  {
+     static unsigned last = 0; 
+     if (millis() - last > 1000) {
+      last = millis();
+      Serial.printf("OPTO 1: %d\n", analogRead(OPTO2));
+     }     
   }
 #endif
 }
