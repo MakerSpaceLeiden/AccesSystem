d6688143944f25ba1f758bcf34d06157d302adc0
Dirk-Willem van Gulik <dirkx@webweaving.org>
2019-10-02 15:00:49 +0200

    make demo/sample Olimex

--- lib-arduino/ACNode/examples/SampleNode/SampleNode.ino
+++ lib-arduino/ACNode/examples/SampleNode/SampleNode.ino
@@ -14,7 +14,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-// #include <PowerNodeV11.h> -- this is an olimex board.
+#include <PowerNodeV11.h> -- this is an olimex board.
 
 #include <WiFiClientSecure.h>
 #include <Wire.h>
