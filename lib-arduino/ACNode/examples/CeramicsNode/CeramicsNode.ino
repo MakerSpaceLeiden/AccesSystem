7a63510c353eaf65586bb68300d26d98a03c1fc5
Dirk-Willem van Gulik <dirkx@webweaving.org>
2024-02-09 17:03:04 +0100

    Updated to new framework; simplified config

--- lib-arduino/ACNode/examples/CeramicsNode/CeramicsNode.ino
+++ lib-arduino/ACNode/examples/CeramicsNode/CeramicsNode.ino
@@ -13,295 +13,186 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
+
+      Compile settings:  EPS32 Dev Module
+      Serial: 460.8kBaud
+
+   Note - if the buzzer 'screams' during boot - you propably selected an
+          ESP32 devboard with a LED that flashes during boot/network traffic.
+          Which is wired to the same wire as the buzzer.
+
 */
+#include <WhiteNodev108.h>
 
-#define WHPULS_GPIO      (4)  // digital in - Wh pulse
-#define CURRENT_GPIO    (24)  // Analog in - current
-#define RELAY_GPIO       (5)  // output
-
-#define AART_LED        (16)  // superfluous indicator LED.
-
-#define REPORTING_PERIOD    (300 * 1000)      // every 5 minutes when on.
-#define REPORTING_PERIOD_OFF  (3600 * 1000)   // every hour when we are off.
-
-// #define RFID_SELECT_PIN (6)
-// #define RFID_RESET_PIN (7)
-
-#define LWIP_DHCP_GET_NTP_SRV 1
-#include "apps/sntp/sntp.h"
-
-#include <ACNode.h>
-// #include <MSL.h>
-// #include <SIG1.h>
-#include <SIG2.h>
-
-#include <Beat.h>
-#include <OTA.h>
-#include <RFID.h>
-
-#include <SyslogStream.h>
-#include <MqttLogStream.h>
-
-#include "/Users/dirkx/.passwd.h"
-
-#ifdef WIFI_NETWORK
-ACNode node = ACNode(WIFI_NETWORK, WIFI_PASSWD);
-#else
-ACNode node = ACNode(true);
+#ifndef MACHINE
+#define MACHINE "ceramicsnode"
 #endif
 
-OTA ota = OTA("FooBar");
-SIG2 sig2 = SIG2();
-Beat beat = Beat();
+// Generate with 'echo -n Password | openssl md5 or
+// use https://www.md5hashgenerator.com/. No \0,
+// cariage return or linefeed  at the end of the
+// password; just the characters of the password
+// itself.
+//
+//#define OTA_PASSWD_MD5  "0f475732f6c1a632b3e161160be0cfc5" // the MD5 of "SomethingSecrit"
 
+#ifndef OTA_PASSWD_HASH
+#error "An OTA password MUST be set as a MD5. Sorry."
+#endif
 
-typedef enum {
-  BOOTING, SWERROR, OUTOFORDER, NOCONN, // some error - machine disabLED.
-  WAITINGFORCARD,             // waiting for card.
-  CHECKINGCARD,
-  POWERED,                    // this is where we engage the relay.
-  REJECTED,
-  POWEROFF,                 // power down an report use.
-  NOTINUSE
-} machinestates_t;
+#define RELAY_NO_START  (OUT0)      // output -- NO is the 'START' signal for the safety contactor
+#define RELAY_NC_STOP   (OUT1)      // output -- NC is part of the interlock/safety circuit
 
-const char *machinestateName[NOTINUSE] = {
-  "Software Error", "Out of order", "No network",
-  "Waiting for card",
-  "Powered",
-  "Rejecting noise/card",
-  "Poweroff",
-  "== not in use == "
-};
+#define OVEN_CURRENT    (CURR0)      // Current of one of the 3 phases running to the oven control relay
+#define WHPULS_GPIO     (CURR1)      // Wh pulse, hard 4k7 pullup to 3v3; open Collector output of kWh meter
 
-unsigned long laststatechange = 0;
-static machinestates_t laststate = OUTOFORDER;
-machinestates_t machinestate = BOOTING;
+#define SAFETY          (OPTO0)     // Wired to the output of the interlock controlled relay/overcurrent switch
+#define VK2000_GPIO     (OPTO1)     // Wired to the second relay on the VK2000 - closes when heater is switched on by VK2000
 
+// Length to 'press' the safety buttons to swith the main safety
+// contactor on or off. Should not be too short; to allow for the
+// damping of the induction pulse.
+//
+#define SAFETY_RELAY_BUTTON_PRESS_LENGTH (300 /* mSeconds */)
 
-unsigned long whCounter = 0, startWhCounter = 0;
-void irqWattHourPulse() {
+// This node is currently not wired; instead it uses an AC/DC convertor
+// and is wired into the same 3P+N+E power as the oven itself.
+//
+WhiteNodev108 node = WhiteNodev108(MACHINE, WIFI_NETWORK, WIFI_PASSWD);
+
+// Extra state above 'POWERED' - when the pottery oven is in use as
+// opposed to the safety circuitry being powered.
+//
+MachineState::machinestate_t FIRING;
+
+ButtonDebounce *safetyDetect, *ovenCurrent, * vk2000detect;
+
+unsigned long startWhCounter = 0;
+volatile unsigned long whCounter = 0;
+void IRAM_ATTR irqWattHourPulse() {
   whCounter++;
 }
-
-#define SLOW_PERIOD (300)
-#define FAST_PERIOD (1000)
-typedef enum { NONE, SLOW, HEARTBEAT, FAST, ON } blinkpattern_t;
-blinkpattern_t blinkstate = FAST;
-Ticker aartLed;
-
-
-#ifdef  ESP32
-// The ESP32 does not yet have automatic PWN when its GPIOs are 
-// given an analogeWrite(). So we have fake that for now - until
-// we can ifdef this out at some Espressif SDK version #.
-//
-#define LEDC_TIMER_13_BIT  (13)
-#define LEDC_BASE_FREQ     (5000)
-#define LEDC_CHANNEL_0  (0)
-
-void analogWrite(uint8_t gpio, float value) {
-  static int channel = -1;
-  if (channel == -1) {
-    channel = 0;
-    // Setup timer and attach timer to a led pin
-    ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
-    ledcAttachPin(gpio, LEDC_CHANNEL_0);
-  }
-
-  // calculate duty, 8191 from 2 ^ 13 - 1
-  uint32_t duty = ((1 << LEDC_TIMER_13_BIT) - 1) * value;
-
-  // write duty to LEDC
-  ledcWrite(channel, duty);
-}
-#endif
-
-void blink() {
-  switch (blinkstate) {
-    case NONE:
-      digitalWrite(AART_LED, 0);
-      break;
-    case SLOW:
-      digitalWrite(AART_LED, (((int)((millis() / SLOW_PERIOD))) & 1));
-      break;
-    case HEARTBEAT:
-      {
-        int ramp = ((int)((millis() / SLOW_PERIOD))) % 30;
-        if (ramp > 15)
-          ramp = 30 - ramp;
-        analogWrite(AART_LED,  (ramp / 15.));
-      };
-      break;
-    case ON:
-      digitalWrite(AART_LED, 1);
-      break;
-    case FAST: // fall throguh to default
-    default:
-      digitalWrite(AART_LED, (((int)((millis() / FAST_PERIOD))) & 1 ));
-      break;
-  }
-};
-
-
-void reportUse(bool isFinal) {
-  if (isFinal)
-    Log.printf("Final %.1f; by %s\n", (whCounter - startWhCounter) / 1000., "xxx");
-  else if (machinestate == POWERED)
-    Log.printf("Use %.1f, kWh Counter: %.1f\n", (whCounter - startWhCounter) / 1000., whCounter / 1000.);
-  else
-    Log.printf("idle, kWh Counter: %.1f\n", whCounter / 1000.);
-}
-
 
 void setup() {
   Serial.begin(115200);
   Serial.println("\n\n\n");
-  Serial.println("Booted: " __FILE__ " " __DATE__ " " __TIME__ );
+  Serial.println("Booted: " __FILE__ " " __DATE__ " " __TIME__);
 
   // Init the hardware and get it into a safe state.
   //
-  pinMode(RELAY_GPIO, OUTPUT);
-  digitalWrite(RELAY_GPIO, 0);
+  digitalWrite(RELAY_NO_START, LOW);
+  pinMode(RELAY_NO_START, OUTPUT);
+  digitalWrite(RELAY_NO_START, LOW);
 
-  pinMode(AART_LED, OUTPUT);
-  digitalWrite(AART_LED, 0);
+  digitalWrite(RELAY_NC_STOP, LOW);
+  pinMode(RELAY_NC_STOP, OUTPUT);
+  digitalWrite(RELAY_NC_STOP, LOW);
 
-  pinMode(CURRENT_GPIO, INPUT);
+  FIRING = node.machinestate.addState("Firing", LED::LED_ON, 60 * 60 * 1000, POWERED);
+
+  pinMode(VK2000_GPIO, INPUT);
+  vk2000detect = new ButtonDebounce(VK2000_GPIO);
+
+  vk2000detect->setCallback([](const int newState) {
+    Log.printf("VK2000 output now %s\n", newState ? "OFF" : "ON");
+  },
+  CHANGE);
+
+  pinMode(SAFETY, INPUT);
+  safetyDetect = new ButtonDebounce(SAFETY);
+  safetyDetect->setCallback([](const int newState) {
+    Log.printf("Interlock power now %s\n", newState ? "OFF" : "ON");
+    if (node.machinestate == MachineState::CHECKINGCARD && !newState) {
+      Log.printf("Machine turned on by tag swipe.\n");
+    }
+    else if (node.machinestate == POWERED && newState) {
+      Log.printf("Machine turned off\n");
+    };
+
+    // We are not policing the state here yet
+    node.machinestate = newState ?  POWERED : MachineState::WAITINGFORCARD;
+  },
+  CHANGE);
+
   pinMode(WHPULS_GPIO, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(WHPULS_GPIO), irqWattHourPulse, FALLING);
 
-#define Strncpy(dst,src) { strncpy(dst,src,sizeof(dst)); }
-  Strncpy(_acnode->moi, "test-ceramitcs");
-  Strncpy(_acnode->mqtt_server, "space.vijn.org");
-  Strncpy(_acnode->machine, "test-ceramitcs");
-  Strncpy(_acnode->master, "test-master");
-  Strncpy(_acnode->logpath, "log");
-  Strncpy(_acnode->mqtt_topic_prefix, "test");
+  ovenCurrent = new ButtonDebounce(OVEN_CURRENT);
+  ovenCurrent->setAnalogThreshold(600);
+  ovenCurrent->setCallback([](const int newState) {
+    Log.printf("Current to heating coil now %s\n", newState ? "OFF" : "ON");
+  },
+  CHANGE);
 
-  aartLed.attach(100, blink);
+  node.setOffCallback([](const int newState) {
+    if (node.machinestate == POWERED) {
+      Log.println("Switching off power");
 
-  node.onConnect([]() {
-    Log.println("Connected");
-    machinestate = WAITINGFORCARD;
+      // Break the safety interlock to power everything off. The
+      // equivalent of pressing the red stop button.
+      //
+      digitalWrite(RELAY_NC_STOP, HIGH);
+      delay(SAFETY_RELAY_BUTTON_PRESS_LENGTH);
+      digitalWrite(RELAY_NC_STOP, LOW);
+
+      // We rely on the change-stage callback to see the
+      // power drop - and then change the state there.
+      return;
+    };
+    Debug.printf("Left button %s ignored.", newState ? "release" : "press");
+  },
+  WHEN_PRESSED);
+
+  node.setOTAPasswordHash(OTA_PASSWD_HASH);
+  node.set_mqtt_prefix("ac");
+  node.set_master("master");
+
+  node.onReport([](JsonObject & report) {
+    report["WhCounter"] = whCounter;
+    report["fw"] = __FILE__ " " __DATE__ " " __TIME__;
   });
-  node.onDisconnect([]() {
-    Log.println("Disconnected");
-    machinestate = NOCONN;
-  });
-  node.onError([](acnode_error_t err) {
-    Log.printf("Error %d\n", err);
-    machinestate = WAITINGFORCARD;
-  });
-
-  node.onValidatedCmd([](const char *cmd, const char *restl) {
-    if (!strcasecmp("energize", cmd)) {
-      machinestate = POWERED;
-      startWhCounter = whCounter;
-    }
-    else if (!strcasecmp("report", cmd)) {
-      reportUse(false);
-    }
-    else if (!strcasecmp("denied", cmd)) {
-      machinestate = REJECTED;
-    } else {
-      Log.printf("Unhandled command <%s> -- ignored.", cmd);
-    }
-  });
-  node.addHandler(&ota);
-
-#ifdef RFID_SELECT_PIN
-   RFID reader(RFID_SELECT_PIN, RFID_RESET_PIN);
-  reader.onSwipe([](const char * tag) {
-    Log.printf("Card <%s> wiped - being checked.\n", tag);
-    machinestate = CHECKINGCARD;
-  });
-  node.addHandler(reader);
-#endif
-
-  // SIG1 sig1 = SIG1();
-  // node.addSecurityHandler(SIG1());
-  node.addSecurityHandler(&sig2);
-  node.addSecurityHandler(&beat);
-
-  // default syslog port and destination (gateway address or broadcast address).
-  //
-  SyslogStream syslogStream = SyslogStream();
-  Debug.addPrintStream(std::make_shared<SyslogStream>(syslogStream));
-  Log.addPrintStream(std::make_shared<SyslogStream>(syslogStream));
-
-  // assumes the client connection for MQTT (and network, etc) is up - otherwise silenty fails/buffers.
-  //
-  MqttLogStream mqttlogStream = MqttLogStream("test", "moi");
-  Log.addPrintStream(std::make_shared<MqttLogStream>(mqttlogStream));
-
-  machinestate = BOOTING;
-
-  node.set_debug(true);
-  node.set_debugAlive(true);
 
   node.begin();
+  node.setOnChangeCallback(MachineState::ALL_STATES, [](MachineState::machinestate_t last, MachineState::machinestate_t current) -> void {
+    if (current == FIRING || current == POWERED)
+      node.updateDisplay("OFF", "", true);
+  });
+
+  node.onApproval([](const char *machine) {
+    Log.println("Approval callback");
+
+    if (node.machinestate != MachineState::CHECKINGCARD) {
+      Log.println("Ignoring approval - not expected.");
+      node.buzzerErr();
+      return;
+    };
+
+    // Jumper the safety interlock to switch things on; the equivalent
+    // of pressing the 'green' button.
+    //
+    digitalWrite(RELAY_NO_START, HIGH);
+    delay(SAFETY_RELAY_BUTTON_PRESS_LENGTH);
+    digitalWrite(RELAY_NO_START, LOW);
+
+    // We rely on the change-state callback to see the
+    // power on the opto that is connected to the output
+    // of all contactor & current-limited; and have that
+    // trigger the change change to 'powered'
+  });
+
+  Log.println("Starting loop(): " __FILE__ " " __DATE__ " " __TIME__);
 }
 
 void loop() {
   node.loop();
 
-  if (laststate != machinestate) {
-    laststate = machinestate;
-    laststatechange = millis();
+#ifdef FAKE
+  static unsigned long lst = millis();
+  static unsigned long cnt = 0;
+  if (millis() - lst > 1000 && cnt != whCounter) {
+    lst = millis(); cnt = whCounter;
+    Log.printf("kWh meter: %.3f\n", whCounter / 1000.);
+    node.buzzerOk();
   }
-
-  switch (machinestate) {
-    case WAITINGFORCARD:
-      digitalWrite(RELAY_GPIO, 0);
-      blinkstate = HEARTBEAT;
-      break;
-
-    case CHECKINGCARD:
-      digitalWrite(AART_LED, ((millis() % 500) < 100) ? 1 : 0);
-      blinkstate = SLOW;
-      if ((millis() - laststatechange) > 5000)
-        machinestate = REJECTED;
-      break;
-
-    case POWERED:
-      blinkstate = ON;
-      digitalWrite(RELAY_GPIO, 1);
-      digitalWrite(AART_LED, 1);
-      if ((millis() - laststatechange) > 5000)
-        machinestate = WAITINGFORCARD;
-      break;
-
-    case REJECTED:
-      digitalWrite(RELAY_GPIO, 0);
-      machinestate = WAITINGFORCARD;
-      break;
-
-    case POWEROFF:
-      digitalWrite(RELAY_GPIO, 0);
-      reportUse(true);
-      machinestate = WAITINGFORCARD;
-      break;
-
-    case NOCONN:
-      blinkstate = FAST;
-
-      if ((millis() - laststatechange) > 120 * 1000) {
-        node.delayedReboot();
-      }
-      break;
-
-    case BOOTING:
-    case OUTOFORDER:
-    case SWERROR:
-    case NOTINUSE:
-      blinkstate = FAST;
-      break;
-  };
-
-  static unsigned long lastReportUse = 0;
-  if (millis() - lastReportUse > ((machinestate == POWERED) ? REPORTING_PERIOD : REPORTING_PERIOD_OFF)) {
-    reportUse(false);
-  };
+#endif
 }
-
