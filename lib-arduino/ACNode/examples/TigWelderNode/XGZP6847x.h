279d1ede3ccc62a9ee954fab3f88d84c373003b8
Dirk-Willem van Gulik <dirkx@webweaving.org>
2024-09-22 21:07:11 +0200

    cleanup io, add prssure sensor, move eeprom out

---
+++ lib-arduino/ACNode/examples/TigWelderNode/XGZP6847x.h
@@ -0,0 +1,90 @@
+#include <Wire.h>
+
+#define XGZP6897D_DEFAULT_ADDR (0x6d) // i2c default address
+
+#define CMD (0x30)   // conversion command
+#define SCO (1<<3)   // conversion complete/requested bit
+#define BOTH (0b010) // which conversions to run (both, no sleep)
+#define PRESS (0x06) // regiser for pressure, 3 long, big endian, two complement
+#define TMEMP (0x09) // registerfor temperature, 2 long, big endian, two complement
+
+class XGZP6897x {
+public:
+    // For K - see page 9 of datasheet; https://cfsensor.com/wp-content/uploads/2022/11/XGZP6847D-Pressure-Sensor-V2.5.pdf
+    //
+    // Pressure        K
+    // range(kpa)  (divisor)
+    // 500<P≤1000      8
+    // 260<P≤500      16
+    // 130<P≤260      32
+    // 65<P≤130       64
+    // 32<P≤65       128
+    // 16<P≤32       256
+    // 8<P≤16        512
+    // 4<P≤8        1024
+    // 2≤P≤4        2048
+    // 1≤P<2        4096
+    //
+    XGZP6897x(uint16_t Kdiv, unsigned char i2caddr = XGZP6897D_DEFAULT_ADDR,  TwoWire* wire = &Wire) : _wire(wire), _K(Kdiv), _i2caddr(i2caddr) {};
+    
+    float getTemperatureInC() {
+        _readBoth();
+        return _temperature;
+    };
+    
+    float getPressureInPa() {
+        _readBoth();
+        return _pressure;
+    };
+private:
+    unsigned char _i2caddr;
+    TwoWire * _wire;
+    unsigned long _K;
+    float _temperature, _pressure;
+    
+    void _startConversion() {
+        _wire->beginTransmission(_i2caddr);
+        _wire->write(CMD);
+        _wire->write(SCO | BOTH);
+        _wire->endTransmission();
+    };
+    
+    bool _checkConversion() {
+        _wire->beginTransmission(_i2caddr);
+        _wire->write(CMD);
+        _wire->endTransmission();
+        _wire->requestFrom(_i2caddr, byte(1));
+        return (_wire->read() & SCO);
+    };
+    
+    void _readBoth() {
+        _startConversion();
+        
+        // conversion should take 20 mSeconds; so
+        // bail out after 40.
+        //
+        for(int i = 0; !_checkConversion(); i++) {
+            if (i > 7) {
+                _pressure = _temperature = 0;
+                return;
+            }
+            delay(5);
+        };
+        
+        _wire->beginTransmission(_i2caddr);
+        _wire->write(PRESS); // pressure is the first 3 byts; so reading 5 byts from here also gets the two for temperature.
+        _wire->endTransmission();
+
+        _wire->requestFrom(_i2caddr, 3+2);
+        unsigned char buff[5];
+        for(int i = 0; i < 5; i++)
+            buff[i] = _wire->read();
+        
+        int32_t p = (buff[0] << 24) | (buff[1] << 16) | (buff[2] << 8); // big endian order, preserve two complement
+        p >>= 8;
+        int16_t t = (buff[3] <<  8) | (buff[4]);      // big endian order, two complement
+        
+        _pressure = float(p) / _K;
+        _temperature = float(t) / 256.;
+    };
+}
