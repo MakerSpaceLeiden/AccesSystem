766c45f54e7712b9aa2bab47b9b08278e51530af
Dirk-Willem van Gulik <dirkx@webweaving.org>
2025-09-28 20:53:18 +0200

    Simple singleton for randomness; now newer MBEDTLS routines require this as an argument

---
+++ lib-arduino/ACNode/src/REST/rnd.cpp
@@ -0,0 +1,35 @@
+#include "ACNode.h"
+#include <cstring>
+
+#include "esp_random.h"
+#include "mbedtls/error.h"
+
+#include "rnd.h"
+
+static mbedtls_entropy_context entropy_ctx;
+mbedtls_ctr_drbg_context ctr_drbg, *p_ctr_drbg;
+
+const char TAG[] ="rnd";
+static const char seed[] = "XXXX" __DATE__ __TIME__;
+
+void ensure_rnd() {
+    if (p_ctr_drbg != NULL)
+        return;
+
+    mbedtls_entropy_init( &entropy_ctx);
+    mbedtls_ctr_drbg_init( &ctr_drbg);
+
+    *(uint32_t*)seed = esp_random();
+
+    int ret;
+    if ((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func,
+                               &entropy_ctx, (const unsigned char *) seed, strlen(seed))) < 0) {
+	char buf[256];
+	mbedtls_strerror( ret, buf, sizeof(buf) ); \
+	ESP_LOGE(TAG, "ensure_rnd() failed. returned -x%02x, %s", (unsigned int) - ret, buf); \
+        return;
+    };
+
+    p_ctr_drbg = &ctr_drbg;
+};
+
---
+++ lib-arduino/ACNode/src/REST/rnd.h
@@ -0,0 +1,8 @@
+#ifndef _H_RND
+#define _H_RND
+#include <mbedtls/entropy.h>
+#include <mbedtls/ctr_drbg.h>
+
+extern mbedtls_ctr_drbg_context ctr_drbg, *p_ctr_drbg;
+void ensure_rnd();
+#endif
