b53d41cafaea784ec69e0e5fe5511522dbaa4bf3
Dirk-Willem van Gulik <dirkx@webweaving.org>
2025-10-29 14:44:52 +0100

    Reduce output

--- lib-arduino/ACNode/src/RFID.cpp
+++ lib-arduino/ACNode/src/RFID.cpp
@@ -34,7 +34,7 @@ void RFID::processAndRateLimitCard(unsigned char * bintag, size_t len) {
         
         if (!_swipe_cb || (_swipe_cb(lasttag) != ACNodeBase::CMD_CLAIMED)) {
             // Simple approval request; default is to 'energise' the contactor on 'machine'.
-            Debug.printf("Requesting approval %p (%s)\n", _acnodebase,_acnodebase->name());
+            Debug.printf("Requesting approval\n");
             if (_acnodebase)
                 _acnodebase->request_approval(lasttag);
         };
